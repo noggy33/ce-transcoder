a94db999d1bef550257fdf73c9e191bec804ebc1
noggy33 <78717140+noggy33@users.noreply.github.com>
2022-08-02 16:13:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     convert_item(input_file)
 
     # 3. Upload HLS file to COS
-    bucket_hls = "hls-video"
+    bucket_hls = "hls-videos"
     upload_files(bucket_hls)
     # 4. Delete MP4 & HLS
     
